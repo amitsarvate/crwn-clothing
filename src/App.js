77dbaf0f8070f4ef5576fd3e8b8ccfa557103bc4
Amit Sarvate <amit@sarvate.com>
2021-12-31 19:35:52 -0500

    building out the sign in and sign up page, creating sign in component etc

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
 
         <Route exact path='/' element={<HomePage/>}/>
         <Route path='/shop' element={<ShopPage/>}/>
+        <Route path='/signin' element={<SignInAndSignUp/>}/>
 
       </Routes>
 
